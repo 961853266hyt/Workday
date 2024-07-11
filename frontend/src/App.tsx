88cd8c2f3ab69929fdaefe98befc6e23050a1fc4
huyiteng <yitengh2@illinois.edu>
2024-07-10 22:31:31 -0500

    change directory

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Container } from '@mui/material'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Container>hi</Container>
+    </>
+  )
+}
+
+export default App
