37c204e7ad90e22cba3751302827813a3a28b494
huyiteng <yitengh2@illinois.edu>
2024-07-17 02:09:05 -0500

    Update User.ts

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -5,17 +5,15 @@ interface IUser extends Document {
   email: string;
   password: string;
   role: string;
-  onboardingApplication?: Schema.Types.ObjectId;
+  // onboardingApplication?: Schema.Types.ObjectId;
 }
-
-// const options = { discriminatorKey: 'role', collection: 'users' };
 
 const UserSchema: Schema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true, enum: ['EMP', 'HR'] },
-  onboardingApplication: { type: Schema.Types.ObjectId, ref: 'OnboardingApplication' },
+  // onboardingApplication: { type: Schema.Types.ObjectId, ref: 'OnboardingApplication' },
 });
 
 const User = mongoose.model<IUser>('User', UserSchema);
